b9d4610d95b3632a403687c3ad7bc95ae6dfaa88
ankita chowdhury <ankitachowdhury145@gmail.com>
2025-07-14 10:12:14 +0530

    agentsetup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/lib/auth";
-import HomeView from "@/modules/home/ui/views/home-view";
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 
