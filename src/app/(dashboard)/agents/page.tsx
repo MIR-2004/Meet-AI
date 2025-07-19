47c9cf4b5b9416c27548d941b80f83667bf3d098
0888888888 <anshuraj9905151156@gmail.com>
2025-07-19 20:39:47 +0530

    agents-filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,9 +1,4 @@
-
-import { 
-  AgentsView,
-   AgentsViewError, 
-   AgentsViewLoading 
-  } from "@/modules/agents/ui/views/agents-view";
+import type { SearchParams } from "nuqs";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
@@ -12,8 +7,22 @@ import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-hea
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { 
+  AgentsView,
+   AgentsViewError, 
+   AgentsViewLoading 
+  } from "@/modules/agents/ui/views/agents-view";
+import { loadSearchParams } from "@/modules/agents/params";
 
-const Page = async () => {
+
+
+interface Props {
+  searchParams: Promise<SearchParams>;
+}
+  
+const Page = async ({ searchParams }: Props) => {
+  const filters = await loadSearchParams(searchParams);
+  
   const session = await auth.api.getSession({
       headers:  await headers(),
     });
@@ -23,7 +32,9 @@ const Page = async () => {
     }
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({}));
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+      ...filters,
+    }));
     return( 
       <>
       <AgentsListHeader />
--- src/modules/agents/params.ts
+++ src/modules/agents/params.ts
@@ -1,10 +1,9 @@
-import { parseAsInteger, parseAsString, useQueryStates } from "nuqs";
+import { createLoader, parseAsInteger, parseAsString } from "nuqs/server";
 import { DEFAULT_PAGE } from "@/constants";
 
-export const useAgentsFilters = () => {
-    return useQueryStates({
+export const filtersSearchParams = {
         search: parseAsString.withDefault("").withOptions({ clearOnDefault: true}),
         page: parseAsInteger.withDefault(DEFAULT_PAGE).withOptions({ clearOnDefault: true }),
-    })
 };
-  
+
+export const loadSearchParams = createLoader(filtersSearchParams);
