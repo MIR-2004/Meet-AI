c4a62fa5ea509221deed63d4ca91d5686fef8717
ankita chowdhury <ankitachowdhury145@gmail.com>
2025-07-24 23:52:50 +0530

    agent update delete

--- src/modules/agents/ui/components/agent-id-view-header.tsx
+++ src/modules/agents/ui/components/agent-id-view-header.tsx
@@ -5,8 +5,16 @@ import {
   BreadcrumbLink,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb" ;
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon, PencilIcon,  MoreVerticalIcon } from "lucide-react";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+import {
+   DropdownMenu,
+   DropdownMenuTrigger,
+   DropdownMenuItem,
+   DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
 
 
 interface Props {
@@ -33,11 +41,41 @@ export const AgentIdViewHeader = ({
                         </Link>
                      </BreadcrumbLink>
                   </BreadcrumbItem>
-                  <BreadcrumbSeparator>
+                  <BreadcrumbSeparator className="text-foreground text-xl font-medium [&>svg]:size-4">
                     <ChevronRightIcon/>
                   </BreadcrumbSeparator>
+                       <BreadcrumbItem>
+                     <BreadcrumbLink asChild className="font-medium text-xl text-foreground">
+                        <Link href={`/agents/${agentId}`}>
+                           {agentName}
+                        </Link>
+                     </BreadcrumbLink>
+                  </BreadcrumbItem>
                </BreadcrumbList>
             </Breadcrumb>
+            {/* without modal={false}, the dialog that this dropdown opens cause the website to get unclickable */}
+            <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost">
+                     <MoreVerticalIcon />
+                  </Button>
+                </DropdownMenuTrigger>
+               
+                  <DropdownMenuContent align="end">
+                     <DropdownMenuItem onClick={onEdit}>
+                         <PencilIcon className="size-4 text-black" />
+                         Edit
+                     </DropdownMenuItem>
+
+                      <DropdownMenuItem onClick={onEdit}>
+                         <TrashIcon className="size-4 text-black" />
+                         Delete
+                     </DropdownMenuItem>
+
+
+                  </DropdownMenuContent>
+               
+            </DropdownMenu>
         </div>
      )
 }
