5b057c75ad27ab03315d90d854af1fe8393e4989
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2025-08-08 20:35:12 +0530

    new

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -33,9 +33,9 @@ export async function POST(req:NextRequest) {
         return NextResponse.json({error: "Invalid JSON"}, { status: 400})
     }
 
-    const evenType = (payload as Record<string, unknown>)?.type;
+    const eventType = (payload as Record<string, unknown>)?.type;
 
-    if(evenType === "call.session_started"){
+    if(eventType === "call.session_started"){
         const event = payload as CallSessionStartedEvent;
         const meetingId = event.call.custom?.meetingId;
 
@@ -69,7 +69,7 @@ export async function POST(req:NextRequest) {
 
         const call = streamVideo.video.call("default", meetingId);
         const realtimeClient = await streamVideo.video.connectOpenAi({
-            call: call,
+            call,
             openAiApiKey: process.env.OPENAI_API_KEY!,
             agentUserId: existingAgent.id,
         })
@@ -77,7 +77,7 @@ export async function POST(req:NextRequest) {
         realtimeClient.updateSession({
             instructions: existingAgent.instructions,
         })
-    }else if( evenType === "call.session_participant_left"){
+    }else if( eventType === "call.session_participant_left"){
         const event = payload as CallSessionParticipantLeftEvent;
         const meetingId = event.call_cid.split(":")[1];
 
@@ -87,6 +87,35 @@ export async function POST(req:NextRequest) {
 
         const call = streamVideo.video.call("default", meetingId);
         await call.end();
+    } else if(eventType === "call.session_ended"){
+        const event = payload as CallEndedEvent;
+        const meetingId = event.call.custom?.meetingId;
+
+        if(!meetingId){
+            return NextResponse.json({error: "Missing meetingId"}, {status: 400})
+        }
+
+        await db.update(meetings).set({ status: "processing", endedAt: new Date()}).where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")))
+    }else if(eventType === "call.transcription_ready"){
+        const event = payload as CallTranscriptionReadyEvent;
+        const meetingId = event.call_cid.split(":")[1];
+
+        const [updateMeeting] = await db
+            .update(meetings)
+            .set({
+                transcriptUrl: event.call_transcription.url,
+            }).where(eq(meetings.id, meetingId))
+            .returning();
+
+
+            if(!updateMeeting){
+                return NextResponse.json({error: "Meeting not found"}, {status: 404})
+            }
+    }else if(eventType === "call.recording_ready"){
+        const event  = payload as CallRecordingReadyEvent;
+        const meetingId = event.call_cid.split(":")[1];
+
+        await db.update(meetings).set({recordingUrl: event.call_recording.url}).where(eq(meetings.id, meetingId))
     }
 
     return NextResponse.json({status: "Ok"})
