13d6c0d4432af25b71d1c4e8d44a3c39a072b722
0888888888 <anshuraj9905151156@gmail.com>
2025-08-19 21:48:25 +0530

    error

--- src/modules/meettings/server/procedures.ts
+++ src/modules/meettings/server/procedures.ts
@@ -1,16 +1,98 @@
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import JSONL from "jsonl-parse-stringify";
+import { and, count, desc, eq, getTableColumns, ilike, inArray, sql } from "drizzle-orm";
 import { db } from "@/db";
-import { agents, meetings } from "@/db/schema";
+import { agents, meetings, user } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
-import { MeetingStatus } from "../types";
+import { MeetingStatus, StreamTranscriptItem } from "../types";
 import { streamVideo } from "@/lib/stream-video";
 import { genertateAvatarUri } from "@/lib/avatar";
 
 export const meetingssRouter = createTRPCRouter({
+    getTranscript: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+        const [existingMeeting] = await db
+        .select()
+        .from(meetings)
+        .where(
+            and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        );
+
+        if(!existingMeeting){
+        throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Meeting not found",
+        });
+    }
+
+    if(!existingMeeting.transcriptUrl){
+        return [];
+    }
+      
+    const transcript = await fetch(existingMeeting.transcriptUrl)
+    .then((res) => res.text())
+    .then((text) => JSON.parse<StreamTranscriptItem>(text))
+    .catch(() => {
+        return [];
+    });
+
+    const speakerIds = [
+        ...new Set(transcript.map((item) => item.speakerId)),
+    ];
+
+    const userSpeakers = await db
+    .select()
+    .from(user)
+    .where(inArray(user.id, speakerIds))
+        .then((users) => 
+            users.map((user) => ({
+              ...user,
+              image:
+              user.image ?? genertateAvatarUri({ seed: user.name, variant: "initials" }),
+            }))
+        );
+
+        const agentSpeakers = await db
+        .select()
+        .from(agents)
+        .where(inArray(agents.id, speakerIds))
+        .then((agents) => 
+            agents.map((agent) => ({
+              ...agent,
+              image: genertateAvatarUri({ seed: agent.name, variant: "botttsNeutral" })
+            }))
+        );
+
+        const speakers = [...userSpeakers, ...agentSpeakers];
+        const transcriptWithSpeaker = transcript.map((item) => {
+            const speaker = speakers.find((speaker) => speaker.id === item.speakerId);
+            if (!speaker) {
+                return {
+                    ...item,
+                    user: {
+                        name: "Unknown",
+                        image: genertateAvatarUri({
+                            seed: "Unknown",
+                            variant: "initials",
+                        }),
+                    }
+                };
+            }
+
+            return {
+                ...item,
+                user: {
+                    name: speaker.name,
+                    image: speaker.image,
+                }
+            };
+        });
+        return transcriptWithSpeaker;
+    }),
 
     generateToken: protectedProcedure.mutation(async ({ ctx }) => {
         await streamVideo.upsertUsers([
